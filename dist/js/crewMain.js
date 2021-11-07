b34c0224d188a5dc0980fb2a42a416e736ba30c2
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-08 00:20:30 +0100

    background toggle for dots in crew page

--- dist/js/crewMain.js
+++ dist/js/crewMain.js
@@ -1,1 +1,36 @@
+const dotsFirst = document.querySelector(".dots #first");
+const dotsSecond = document.querySelector(".dots #second");
+const dotsThird = document.querySelector(".dots #third");
+const dotsFourth = document.querySelector(".dots #fourth");
 
+dotsFirst.addEventListener("click", () => {
+  dotsFirst.classList.add("current");
+
+  dotsSecond.classList.remove("current");
+  dotsThird.classList.remove("current");
+  dotsFourth.classList.remove("current");
+});
+
+dotsSecond.addEventListener("click", () => {
+  dotsSecond.classList.add("current");
+
+  dotsFirst.classList.remove("current");
+  dotsThird.classList.remove("current");
+  dotsFourth.classList.remove("current");
+});
+
+dotsThird.addEventListener("click", () => {
+  dotsThird.classList.add("current");
+
+  dotsFirst.classList.remove("current");
+  dotsSecond.classList.remove("current");
+  dotsFourth.classList.remove("current");
+});
+
+dotsFourth.addEventListener("click", () => {
+  dotsFourth.classList.add("current");
+
+  dotsFirst.classList.remove("current");
+  dotsSecond.classList.remove("current");
+  dotsThird.classList.remove("current");
+});
