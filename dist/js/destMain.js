9fd579930217eae4b217d77f3e0110d9c6be37e5
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-08 00:17:43 +0100

    folder structure update

--- dist/js/destMain.js
+++ dist/js/destMain.js
@@ -1,40 +1,3 @@
-const home = document.querySelector("#home");
-const dest = document.querySelector("#dest");
-const crew = document.querySelector("#crew");
-const tech = document.querySelector("#tech");
-
-home.addEventListener("click", (e) => {
-  home.className = "active";
-
-  dest.classList.remove("active");
-  crew.classList.remove("active");
-  tech.classList.remove("active");
-});
-
-dest.addEventListener("click", (e) => {
-  dest.className = "active";
-
-  home.classList.remove("active");
-  crew.classList.remove("active");
-  tech.classList.remove("active");
-});
-
-crew.addEventListener("click", (e) => {
-  crew.className = "active";
-
-  home.classList.remove("active");
-  dest.classList.remove("active");
-  tech.classList.remove("active");
-});
-
-tech.addEventListener("click", (e) => {
-  tech.className = "active";
-
-  home.classList.remove("active");
-  dest.classList.remove("active");
-  crew.classList.remove("active");
-});
-
 const moon = document.querySelector("#moon");
 const mars = document.querySelector("#mars");
 const europa = document.querySelector("#europa");
@@ -58,7 +21,7 @@ moon.addEventListener("click", () => {
     "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.";
   avgDist.innerHTML = "384,400 km";
   estTime.innerHTML = "3 days";
-  planetImg.src = "../images/image-moon.png";
+  planetImg.src = "../../images/image-moon.png";
 });
 
 mars.addEventListener("click", () => {
@@ -73,7 +36,7 @@ mars.addEventListener("click", () => {
     "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!";
   avgDist.innerHTML = "225 MIL. km";
   estTime.innerHTML = "9 months";
-  planetImg.src = "../images/image-mars.png";
+  planetImg.src = "../../images/image-mars.png";
 });
 
 europa.addEventListener("click", () => {
@@ -88,7 +51,7 @@ europa.addEventListener("click", () => {
     "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.";
   avgDist.innerHTML = "628 MIL. km";
   estTime.innerHTML = "3 years";
-  planetImg.src = "../images/image-europa.png";
+  planetImg.src = "../../images/image-europa.png";
 });
 
 titan.addEventListener("click", () => {
@@ -103,5 +66,5 @@ titan.addEventListener("click", () => {
     "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.";
   avgDist.innerHTML = "1.6 BIL. km";
   estTime.innerHTML = "7 years";
-  planetImg.src = "../images/image-titan.png";
+  planetImg.src = "../../images/image-titan.png";
 });
