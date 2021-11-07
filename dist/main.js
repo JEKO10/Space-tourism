57d0a35029548b507322bb831f75a802881ebf21
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-07 22:59:26 +0100

    images added for each planet

--- dist/main.js
+++ dist/main.js
@@ -58,6 +58,7 @@ moon.addEventListener("click", () => {
     "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.";
   avgDist.innerHTML = "384,400 km";
   estTime.innerHTML = "3 days";
+  planetImg.src = "../images/image-moon.png";
 });
 
 mars.addEventListener("click", () => {
@@ -72,6 +73,7 @@ mars.addEventListener("click", () => {
     "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!";
   avgDist.innerHTML = "225 MIL. km";
   estTime.innerHTML = "9 months";
+  planetImg.src = "../images/image-mars.png";
 });
 
 europa.addEventListener("click", () => {
@@ -86,6 +88,7 @@ europa.addEventListener("click", () => {
     "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.";
   avgDist.innerHTML = "628 MIL. km";
   estTime.innerHTML = "3 years";
+  planetImg.src = "../images/image-europa.png";
 });
 
 titan.addEventListener("click", () => {
@@ -100,4 +103,5 @@ titan.addEventListener("click", () => {
     "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.";
   avgDist.innerHTML = "1.6 BIL. km";
   estTime.innerHTML = "7 years";
+  planetImg.src = "../images/image-titan.png";
 });
