0d1c8f2b9e6ff1923f65c7973a6775dfbc1c8a3d
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-08 21:37:01 +0100

    technology page finished

--- dist/js/techMain.js
+++ dist/js/techMain.js
@@ -4,6 +4,7 @@ const three = document.querySelector("#three");
 
 const techName = document.querySelector("#techName");
 const techDescription = document.querySelector("#techDescription");
+const techImg = document.querySelector("#techImg");
 
 one.addEventListener("click", () => {
   one.classList.add("current");
@@ -14,6 +15,7 @@ one.addEventListener("click", () => {
   techName.innerHTML = "Launch Vehicle";
   techDescription.innerHTML =
     "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!";
+  techImg.src = "../../images/launch-vehicle.jpg";
 });
 
 two.addEventListener("click", () => {
@@ -25,6 +27,7 @@ two.addEventListener("click", () => {
   techName.innerHTML = "Spaceport";
   techDescription.innerHTML =
     "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.";
+  techImg.src = "../../images/spaceport.jpg";
 });
 
 three.addEventListener("click", () => {
@@ -36,4 +39,5 @@ three.addEventListener("click", () => {
   techName.innerHTML = "Space Capsule";
   techDescription.innerHTML =
     "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.";
+  techImg.src = "../../images/space-capsule.jpg";
 });
